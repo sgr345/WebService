6bc4a3f65426e4a1c8666d308968d0733c8806af
sgr345 <113155941+sgr345@users.noreply.github.com>
2022-10-02 16:58:16 +0900

    ・フロントViewModel使い分け ・BoardApi追加

---
+++ FrontApp/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrontApp.Models
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+            var test = Request.PathBase;
+        }
+    }
+}
+
---
+++ RestApiServer/Models/DataModel/Board.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace RestApiServer.Models.DataModel
+{
+    public class Board
+    {
+        public int No { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string UserID { get; set; }
+
+        public string UserName { get; set; }
+
+        public int ReadCount { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
+
